c6b4b928bb974ee6c344394be92247985af8da8e
Kasper Mikiewicz <IderedPL@gmail.com>
2013-01-28 16:07:17 +0100

    extend latestTweets.js

---
+++ JS/latestTweets.js
@@ -0,0 +1,12 @@
+/**
+ * Get latest tweets
+ * @param  {string} username
+ * @param  {int} num      Number of tweets to get
+ */
+function getTweets(username, num) {
+
+    $.getJSON("http://api.twitter.com/1/statuses/user_timeline/"+username+".json?count="+num+"&include_rts=1&callback=?", function(tweets) {
+         $('.js-get-tweet').html(parseTweet(tweets[0].text));
+    });
+
+} // getTweets
