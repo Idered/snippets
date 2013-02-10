e2ec9234ca290127ec7df188708f4304c6cce1e9
Kasper Mikiewicz <IderedPL@gmail.com>
2013-02-10 14:02:06 +0100

    update getVendorPrefix.js

--- JS/getVendorPrefix.js
+++ JS/getVendorPrefix.js
@@ -1,32 +1,31 @@
-//Lea Verou's getVendorPrefix()
-//NO WARRANTY EXPRESSED NOR IMPLIED. USE AT YOUR OWN RISK.
-//Origin: AT http://lea.verou.me/2009/02/find-the-vendor-prefix-of-the-current-browser/
+/**
+ * Get vendor prefix
+ *
+ * NO WARRANTY EXPRESSED NOR IMPLIED. USE AT YOUR OWN RISK.
+ *
+ * @author Lea Verou
+ * @see    lea.verou.me/2009/02/find-the-vendor-prefix-of-the-current-browser
+ * @return {string} Vendor prefix
+ */
+function getVendorPrefix() {
 
- 
-function getVendorPrefix()
-{
     if('result' in arguments.callee) return arguments.callee.result;
-    
-    var regex = /^(Moz|Webkit|Khtml|O|ms|Icab)(?=[A-Z])/;
-    
-    var someScript = document.getElementsByTagName('script')[0];
-    
+
+    var regex = /^(Moz|Webkit|Khtml|O|ms|Icab)(?=[A-Z])/,
+        someScript = document.getElementsByTagName('script')[0];
+
     for(var prop in someScript.style)
-    {
         if(regex.test(prop))
-        {
             // test is faster than match, so it's better to perform
             // that on the lot and match only when necessary
             return arguments.callee.result = prop.match(regex)[0];
-        }
-        
-    }
-    
+
     // Nothing found so far? Webkit does not enumerate over the CSS properties of the style object.
     // However (prop in style) returns the correct value, so we'll have to test for
     // the precence of a specific property
     if('WebkitOpacity' in someScript.style) return arguments.callee.result = 'WebKit';
     if('KhtmlOpacity' in someScript.style) return arguments.callee.result = 'Khtml';
-    
+
     return arguments.callee.result = '';
+
 }
