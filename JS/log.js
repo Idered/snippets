a97f262ae0347cf7524f9b1bbce285e89b8d30bd
Kasper Mikiewicz <IderedPL@gmail.com>
2013-02-27 22:01:39 +0100

    add grouped logging

--- JS/log.js
+++ JS/log.js
@@ -8,3 +8,36 @@ function _log() {
     debug && window.console && console.log.apply(console, arguments);
 
 } // log
+
+
+/**
+ * Grouped logging
+ * @param {string} Group name
+ * @param {mixed}  Params to log
+ *
+ * Usage: log('group-name', var1, var2, varN);
+ * Sample: log('user-actions', 'Action: ', action);
+ */
+var debug = ['user-actions'],
+	log = function() {
+	var args = [],
+		first;
+
+	// clone arguments
+	Array.prototype.push.apply(args, arguments);
+
+	// we need at least 2 arguments: group name and variable to log
+	if (args.length > 1) {
+		// get group name
+		first = args.shift();
+
+		// look for group name in debug array
+		for (var i = 0; i < debug.length; i++)
+			if (first === debug[i])
+				console.log.apply(console, args);
+
+	} else {
+		// We have only 1 argument, there's no group name
+		console.log.apply(console, args);
+	}
+};
