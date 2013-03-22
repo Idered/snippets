84569dfba62e5a7766eb5b2a4e71da8efeb124e8
Rafal <gicolek@gmail.com>
2013-03-22 18:27:27 +0100

    Change array call to older php version add response headers

--- PHP/wp.sendMail.php
+++ PHP/wp.sendMail.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Validate data and send mail.
  *
@@ -12,48 +13,46 @@ function sendMail() {
 
 	$response = array(
 		'status' => -2,
-		'errors' => []
+		'errors' => array( ),
 	);
-	
-	if ( ! isset($_POST['name']) || ! isset($_POST['email']) || ! isset($_POST['message']) ) {
-		
-		echo json_encode($response);
-		
+
+	if ( !isset( $_POST['name'] ) || !isset( $_POST['email'] ) || !isset( $_POST['message'] ) ) {
+
+		echo json_encode( $response );
+
 		die();
-		
 	}
 
-	$name = esc_attr($_POST['name']);
-	$email = sanitize_email($_POST['email']);
-	$message = esc_textarea($_POST['message']);
+	$name = esc_attr( $_POST['name'] );
+	$email = sanitize_email( $_POST['email'] );
+	$message = esc_textarea( $_POST['message'] );
 
-	if ( ! strlen($name) )
+	if ( !strlen( $name ) )
 		$response['errors']['name'] = "C'mon, what's your name?";
 
-	if ( ! is_email($email) )
+	if ( !is_email( $email ) )
 		$response['errors']['email'] = "Please, give us valid email.";
 
-	if ( ! strlen($message) )
+	if ( !strlen( $message ) )
 		$response['errors']['message'] = "No message, huh?";
 
-	if ( empty($response['errors']) ) {
+	if ( empty( $response['errors'] ) ) {
 
-		$to = get_bloginfo('admin_email');
-		$subject = 'Contact from ' . get_bloginfo('name');
+		$to = get_bloginfo( 'admin_email' );
+		$subject = 'Contact from ' . get_bloginfo( 'name' );
 
 		$headers[] = "From: $name <$email>";
 
 		$isSent = wp_mail( $to, $subject, $message, $headers );
 
 		$response['status'] = $isSent ? 1 : -1;
-
 	}
 
-	echo json_encode($response);
+	header( "Content-Type: application/json" );
+	echo json_encode( $response );
 
 	die();
 }
-
 
 add_action( 'init', 'sm_ajax_setup' );
 
